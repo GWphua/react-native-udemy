f8db1dc74e249f204aa06f680f1131fcb08fe923
Guan Wei <phuaguanwei99@gmail.com>
2023-02-16 14:13:31 +0800

    Showing and Styling list items

--- App.tsx
+++ App.tsx
@@ -3,13 +3,17 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
 
   const goalInputHandler = (text: string) => {
     setEnteredGoalText(text);
   };
 
   const addGoalHandler = () => {
-    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
   };
 
   return (
@@ -23,7 +27,11 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal, index) => (
+          <Text key={index} style={styles.goalItem}>
+            {goal} {index}
+          </Text>
+        ))}
       </View>
     </View>
   );
@@ -54,5 +62,12 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
   },
 });
