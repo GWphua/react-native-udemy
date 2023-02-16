84cb9f9b044d043ee131d7583239720df92ae691
Guan Wei <phuaguanwei99@gmail.com>
2023-02-16 11:54:55 +0800

    Start off with styles, buttons.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Text style={styles.dummyText}>Inner Text</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title="Tap me!" />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// Helps to clearly seperate styles with TSX code.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  dummyText: { margin: 16, borderWidth: 2, borderColor: "blue", padding: 16 },
 });
